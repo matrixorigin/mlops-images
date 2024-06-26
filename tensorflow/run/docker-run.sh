58bd505dfb8a558fb7a6f96c7bb79444cfff450f
dongshuai <>
2024-06-26 19:03:11 +0800

    Tensorflow镜像构建

---
+++ tensorflow/run/docker-run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# tensorflow:xxx需替换成构建好的实际镜像地址。
+# 运行tensorflow容器，根据主机端口实际情况进行端口映射，如下述：主机2222映射容器ssh的22，主机8888映射容器jupyter的8888，主机6006映射容器TensorBoard端口6006
+docker run  -d  -ti \
+--restart=always \
+--name pytorch-test \
+--gpus all --ipc=host \
+-p 2222:22 \
+-p 8888:8888 \
+-p 6006:6006 \
+tensorflow:2.15.0-python3.8-cuda12.1.0-cudnn8-devel-ubuntu22.04
