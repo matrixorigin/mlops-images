e2f970609acba4566cf327a6f66bfa975af8cdef
dongshuai <>
2024-07-08 20:47:25 +0800

    tensorflow镜像，添加tensorRT的部署

--- tensorflow/build-scripts.sh
+++ tensorflow/build-scripts.sh
@@ -6,6 +6,13 @@
 BASE_IMAGE=nvidia/cuda:12.1.0-cudnn8-devel-ubuntu22.04
 
 PYTHON_VERSION=3.10
+# miniconda的安装包均放在：https://repo.anaconda.com/miniconda/。根据要安装的python版本、操作系统，选择对应的miniconda安装包。
+MINICONDA_PKG=Miniconda3-py310_24.5.0-0-Linux-x86_64.sh
+
+# tensorRT pip安装tensorRT后，需要指定软连接到/usr/lib下，否则会报tensorRT not found
+TENSORRT_VERSION=8.6.1
+CU_VERSION='' # 如果不填默认-cu12，也可指定-cu11
+
 # 从 TensorFlow 2.1 开始，pip 包 tensorflow 即同时包含 GPU 支持，无需通过特定的 pip 包 tensorflow-gpu 安装 GPU 版本。
 # 如果安装tensorflow-gpu:1.x版本,则TENSORFLOW_TYPE设为tensorflow-gpu，TENSORFLOW_VERSION设为1.x
 TENSORFLOW_TYPE=tensorflow
@@ -18,6 +25,9 @@ IMAGE_TAG=${TENSORFLOW_VERSION}-python${PYTHON_VERSION}-cuda12.1.0-cudnn8-devel-
 docker build \
     --build-arg BASE_IMAGE=${BASE_IMAGE} \
     --build-arg PYTHON_VERSION=${PYTHON_VERSION} \
+    --build-arg MINICONDA_PKG=${MINICONDA_PKG} \
+    --build-arg TENSORRT_VERSION=${TENSORRT_VERSION} \
+    --build-arg CU_VERSION=${CU_VERSION} \
     --build-arg TENSORFLOW_TYPE=${TENSORFLOW_TYPE} \
     --build-arg TENSORFLOW_VERSION=${TENSORFLOW_VERSION} \
     -t tensorflow:${IMAGE_TAG} \
