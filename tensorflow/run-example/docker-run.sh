cc330c1cee23d75ab0ea677e118464f3ea816f11
dongshuai <>
2024-07-30 11:58:42 +0800

    安装工具git 和 ping

--- tensorflow/run-example/docker-run.sh
+++ tensorflow/run-example/docker-run.sh
@@ -3,9 +3,9 @@
 # 运行tensorflow容器，根据主机端口实际情况进行端口映射，如下述：主机2222映射容器ssh的22，主机8888映射容器jupyter的8888，主机6006映射容器TensorBoard端口6006
 docker run  -d  -ti \
 --restart=always \
---name pytorch-test \
+--name tensorflow-test \
 --gpus all --ipc=host \
 -p 2222:22 \
 -p 8888:8888 \
 -p 6006:6006 \
-tensorflow:2.15.0-python3.8-cuda12.1.0-cudnn8-devel-ubuntu22.04
+tensorflow:2.15.0-python3.10-cuda12.1.0-cudnn8-devel-ubuntu22.04
