cd2dc0f9f7cec6a861ebe32a2af03aff6b331848
areyanGIT <tester_dream@163.com>
2024-08-02 11:21:57 +0800

    增加h100使用comment

--- common/init/boot/boot.sh
+++ common/init/boot/boot.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "begin download start.sh file" > /tmp/boot.log
 rm -rf /tmp/gpuhub && mkdir  /tmp/gpuhub
+# 如果是要泡在h100机器上，http://sharefile.neolink.com/file/start.sh需要改成http://sharefile.neolink.com/file/start-h00.sh
 curl --connect-timeout 10 -o /tmp/gpuhub/start.sh http://sharefile.neolink.com/file/start.sh -k -s
 
 echo "download start.sh script finished" >> /tmp/boot.log
