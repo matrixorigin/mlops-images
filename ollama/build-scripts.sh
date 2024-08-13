bacf7f2d091ea54a32be8a7e995ab1380eff5e7c
Ian Chenyang Zhao <chzhao8904@gmail.com>
2024-08-13 18:58:09 +0800

    use webui file in ollma image, provide webui

--- ollama/build-scripts.sh
+++ ollama/build-scripts.sh
@@ -3,7 +3,6 @@
 # 按照自己的需要，是否支持GPU/CUDA版本等选择基础镜像
 # 如果是用构建支持GPU的，使用nvidia/cuda作为基础镜像；如果仅支持CPU，ubuntu作为基础镜像
 # 例如：GPU的：nvidia/cuda:12.1.0-cudnn8-devel-ubuntu22.04，仅支持CPU的：ubuntu22.04
-
 BASE_IMAGE=ollama/ollama
 
 PYTHON_VERSION=3.10
@@ -13,10 +12,16 @@ MINICONDA_PKG=Miniconda3-py310_24.5.0-0-Linux-x86_64.sh
 # 构建后的镜像tag，需要体现pytorch、python、基础镜像版本信息
 IMAGE_TAG=python${PYTHON_VERSION}-cuda12.1.0-cudnn8-devel-ubuntu22.04
 
+# 复制init 文件
+cp -r ../common/init ./init
+
 docker build \
-    --build-arg BASE_IMAGE=${BASE_IMAGE} \
-    --build-arg PYTHON_VERSION=${PYTHON_VERSION} \
-    --build-arg MINICONDA_PKG=${MINICONDA_PKG} \
-    -t ollama-webui:${IMAGE_TAG}\
-    -f ./Dockerfile \
-    .
+  --build-arg BASE_IMAGE=${BASE_IMAGE} \
+  --build-arg PYTHON_VERSION=${PYTHON_VERSION} \
+  --build-arg MINICONDA_PKG=${MINICONDA_PKG} \
+  -t ollama-webui:${IMAGE_TAG} \
+  -f ./Dockerfile \
+  .
+
+#  移出init
+rm -rf ./init
