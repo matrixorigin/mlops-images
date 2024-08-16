efb9d1b361cc5ba341c197951ea8c72b8bbb1982
randomradio <chzhao8904@gmail.com>
2024-08-16 16:44:26 +0800

    fix ollama image starting issue. use pre-built ollama bin and nvidia-docker image.

--- ollama/build-scripts.sh
+++ ollama/build-scripts.sh
@@ -3,7 +3,7 @@
 # 按照自己的需要，是否支持GPU/CUDA版本等选择基础镜像
 # 如果是用构建支持GPU的，使用nvidia/cuda作为基础镜像；如果仅支持CPU，ubuntu作为基础镜像
 # 例如：GPU的：nvidia/cuda:12.1.0-cudnn8-devel-ubuntu22.04，仅支持CPU的：ubuntu22.04
-BASE_IMAGE=ollama/ollama
+BASE_IMAGE=nvidia/cuda:12.1.0-cudnn8-devel-ubuntu22.04
 
 PYTHON_VERSION=3.10
 # miniconda的安装包均放在：https://repo.anaconda.com/miniconda/。根据要安装的python版本、操作系统，选择对应的miniconda安装包。
