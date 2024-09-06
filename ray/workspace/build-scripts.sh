57a568b1e71d8da0aaccdce77a5fc358d599f583
bincherry <53431275+bincherry@users.noreply.github.com>
2024-09-06 18:34:45 +0800

    feat: add image for ray

---
+++ ray/workspace/build-scripts.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# 按照自己的需要，是否支持GPU/CUDA版本等选择基础镜像
+# 如果是用构建支持GPU的，使用nvidia/cuda作为基础镜像；如果仅支持CPU，ubuntu作为基础镜像
+# 例如：GPU的：nvidia/cuda:12.1.0-cudnn8-devel-ubuntu22.04，仅支持CPU的：ubuntu22.04
+
+BASE_IMAGE=ubuntu:22.04
+
+PYTHON_VERSION=3.10
+RAY_VERSION=2.35.0
+# miniconda的安装包均放在：https://repo.anaconda.com/miniconda/。根据要安装的miniconda版本、python版本、操作系统，选择对应的miniconda安装包。
+MINICONDA_PKG=Miniconda3-py310_24.5.0-0-Linux-x86_64.sh
+
+# 构建后的镜像tag，需要体现pytorch、python、基础镜像版本信息
+IMAGE_TAG=workspace-${RAY_VERSION}-python${PYTHON_VERSION}-ubuntu22.04
+
+REGISTRY=images.neolink-ai.com/matrixdc
+
+DOCKER_BUILDKIT=1
+
+EXTRA_ARGS="$@"
+
+docker buildx build \
+    --platform=linux/amd64 \
+    --build-arg BASE_IMAGE=${BASE_IMAGE} \
+    --build-arg MINICONDA_PKG=${MINICONDA_PKG} \
+    --build-arg RAY_VERSION=${RAY_VERSION} \
+    $EXTRA_ARGS \
+    -t ${REGISTRY}/ray:${IMAGE_TAG} \
+    -f ./Dockerfile \
+    .
