19091bcf63dc129224389d6cd55dfbe8768e8910
chenyangzhao <chzhao8904@gmail.com>
2024-09-14 19:44:40 +0800

    reduce comfy ui size without downloading all model and plugins

--- comfyui/build-scripts.sh
+++ comfyui/build-scripts.sh
@@ -4,7 +4,7 @@
 BASE_IMAGE=nvidia/cuda:12.1.0-cudnn8-devel-ubuntu22.04
 # 构建后的镜像 tag
 
-IMAGE_TAG=opensuse/tumbleweed:latest
+IMAGE_TAG=comfyui-megapack-base-cu12.1-cudnn8-ubuntu22.04
 
 # Docker build 命令
 docker buildx build --platform linux/amd64 \
