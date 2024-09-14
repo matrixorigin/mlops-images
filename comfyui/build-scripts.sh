637b5536b8508e82ae5bf644a3e344200998a7a3
chenyangzhao <chzhao8904@gmail.com>
2024-09-14 23:51:13 +0800

    fix image tag in build-scripts.sh

--- comfyui/build-scripts.sh
+++ comfyui/build-scripts.sh
@@ -4,12 +4,12 @@
 BASE_IMAGE=nvidia/cuda:12.1.0-cudnn8-devel-ubuntu22.04
 # 构建后的镜像 tag
 
-BASE_IMAGE=nvidia/cuda:12.1.0-cudnn8-devel-ubuntu22.04
+IMAGE_TAG=comfyui-cuda:12.1.0-cudnn8-devel-ubuntu22.04
 
 # 复制init 文件
 cp -r ../common/init ./init
 # Docker build 命令
-docker buildx build --platform linux/amd64 \
+docker build --platform linux/amd64 \
     --build-arg BASE_IMAGE=${BASE_IMAGE} \
     -t ${IMAGE_TAG} \
     .
